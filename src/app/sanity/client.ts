6949ee8212d07b6c40b84686de2fa82ce9babb31
Francis3560 <kushkiddeny11@gmail.com>
2025-10-11 17:55:24 +0300

    changed credentials

--- src/app/sanity/client.ts
+++ src/app/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "8l26zfp0",
+  projectId: "kgiyfrzm",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
